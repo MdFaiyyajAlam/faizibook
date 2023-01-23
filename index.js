70cd0ffe0ab11c0261d54b259b78b0453fdbc3be
Md Faiyyaj Alam <mdfaiyyajalam54918@gmail.com>
2023-01-23 10:53:05 +0530

    3rd commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ useNewUrlParser:true,
 useUnifiedTopology:true
 }).then(() => {
     console.log("connection successful");
-}).catch((e) => {
-    console.log(e);
+}).catch((error) => {
+    console.log(error);
 })
 
 const port = process.env.PORT || 3000;
